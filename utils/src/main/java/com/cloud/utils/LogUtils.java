3eab516189fb6b334751328cca3adf0870819ca2
Dajeong-Park <ekwjd7182@gmail.com>
2023-10-26 13:18:33 +0900

    로그 테스트

--- utils/src/main/java/com/cloud/utils/LogUtils.java
+++ utils/src/main/java/com/cloud/utils/LogUtils.java
@@ -29,9 +29,11 @@ import java.util.Set;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.logging.log4j.core.appender.FileAppender;
 import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 
 import com.google.gson.Gson;
 
@@ -61,8 +63,12 @@ public class LogUtils {
     }
     public static Set<String> getLogFileNames() {
         Set<String> fileNames = new HashSet<>();
-        org.apache.logging.log4j.core.Logger rootLogger = (org.apache.logging.log4j.core.Logger)LogManager.getRootLogger();
-        Map<String, Appender> appenderMap = rootLogger.getAppenders();
+        LoggerContext context = (LoggerContext) LogManager.getContext(false);
+        org.apache.logging.log4j.core.config.Configuration configuration = context.getConfiguration();
+        LoggerConfig loggerConfig = configuration.getRootLogger();
+        Map<String, Appender> appenderMap = loggerConfig.getAppenders();
+        // org.apache.logging.log4j.core.Logger rootLogger = (org.apache.logging.log4j.core.Logger)LogManager.getRootLogger();
+        // Map<String, Appender> appenderMap = rootLogger.getAppenders();
         int appenderCount = 0;
         for (Appender appender : appenderMap.values()){
             ++appenderCount;
